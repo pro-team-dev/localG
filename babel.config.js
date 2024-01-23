e5c81b3896800e1d77ad62e6544871a75f1a75eb
EV-OD <evod599@gmail.com>
2024-01-23 17:43:27 +0545

    test2

--- babel.config.js
+++ babel.config.js
@@ -6,17 +6,6 @@ module.exports = function (api) {
       // Required for expo-router
       "expo-router/babel",
       "nativewind/babel",
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "envBase",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
     ],
   };
 };
